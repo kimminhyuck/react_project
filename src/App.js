da86f8d13611fbcfbc75982b37d778e3ced6fa12
kimminhyuck <rhkddka02@naver.com>
2024-12-26 12:34:19 +0900

    12시 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {
+  console.log('props',props.title);
+  return (
+    <header>
+      <h1><a href="/">React</a></h1>
+    </header>
+  );
+}
+
+function Article(props){
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.desc}
+    </article>
+  );
+}
+
+function Nav(props) {
+  const lis = []
+    for (let i=0; i<props.topics.length; i++){
+      let t = props.topics[i];
+      lis.push(<li>{t.title}</li>);
+    }
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  )
+}
+
 function App() {
+  const topics = [
+    {id:1, title:'html', desc:'html is ...'},
+    {id:2, title:'css', desc:'css is ...'},
+    {id:3, title:'js', desc:'js iavascript is ...'}
+  ];
   return (
     <div>
-      <header>
-        <h1><a href="/">WEB</a></h1>
-      </header>
-      <nav>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-          <li><a href="/read/3">js</a></li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB!
-      </article>
+      <Header title="REACT"></Header>
+       <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
 }
