8117a6a1e72c1d7b6ace4bd8ce2da8c36317052e
kimminhyuck <rhkddka02@naver.com>
2024-12-26 12:57:40 +0900

    리엑트 1주차

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-// import logo from './logo.svg';
 import './App.css';
 
 function Header(props) {
-  console.log('props',props.title);
+  
   return (
     <header>
-      <h1><a href="/">React</a></h1>
+      <h1><a href="/" onClick={(event) => {
+        event.preventDefault()
+        props.onChangeMode()
+      }}>{props.title}</a></h1>
     </header>
-  );
-}
-
-function Article(props){
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      {props.desc}
-    </article>
-  );
+  )
 }
 
 function Nav(props) {
+  
   const lis = []
-    for (let i=0; i<props.topics.length; i++){
-      let t = props.topics[i];
-      lis.push(<li>{t.title}</li>);
-    }
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={event => {
+        event.preventDefault()
+        props.onChangeMode(event.target.id)
+      }}>{t.title}</a>
+    </li>)
+  }
   return (
     <nav>
       <ol>
@@ -34,16 +33,29 @@ function Nav(props) {
   )
 }
 
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
+
 function App() {
   const topics = [
-    {id:1, title:'html', desc:'html is ...'},
-    {id:2, title:'css', desc:'css is ...'},
-    {id:3, title:'js', desc:'js iavascript is ...'}
-  ];
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javascript', body: 'javascript is ...' }
+  ]
   return (
     <div>
-      <Header title="REACT"></Header>
-       <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={() => {
+        alert('Header')
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id)
+      }}></Nav>
       <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
