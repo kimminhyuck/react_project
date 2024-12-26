b0f60378492377792fc961b4f763c9715f1dc3a9
kimminhyuck <rhkddka02@naver.com>
2024-12-26 11:48:50 +0900

    리엑트 1주차

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello React</h2>
-
+    <div>
+      <header>
+        <h1><a href="/">WEB</a></h1>
+      </header>
+      <nav>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+          <li><a href="/read/3">js</a></li>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB!
+      </article>
     </div>
   );
 }
