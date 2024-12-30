56e3cedfe71f64080820c21cb7e5ee1301b5353e
kimminhyuck <rhkddka02@naver.com>
2024-12-30 10:23:15 +0900

    10시 23분 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { createStore } from "redux"
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const counterSlice = createSlice({
+  name: 'counterSlice',
+  initialState: { value: 0 },
+  reducers: {
+    up: (state, action) => {
+      state.value = state.value + action.step
+    }
+  }
+})
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer
+  }
+})
+
+// function reducer(state, action) {
+//   if (action.type === 'up') {
+//     return { ...state, value: state.value + action.step }
+//   }
+//   return state;
+// }
+// const initialState = { value: 0 }
+// const store = createStore(reducer, initialState);
+
+function Counter() {
+  const dispatch = useDispatch();
+  const count = useSelector(state => {
+    console.log(state)
+    state.value
+  })
+  return <div>
+    <button onClick={() => {
+      dispatch({ type: 'up', step: 2 })
+    }}>+</button> {count}
+  </div>
 }
 
-export default App;
+export default function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <Counter></Counter>
+      </div>
+    </Provider>
+  )
+}
