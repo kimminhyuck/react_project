131c6d1a93af4b93d1169f87840d620a56a27535
kimminhyuck <rhkddka02@naver.com>
2025-01-03 17:24:49 +0900

    오늘은여기까지

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,39 +4,7 @@ import Editor from "../component/Editor";
 
 const Home = () => {
   return (
-    <div>
-      <Header
-        title={"Home"}
-        leftChild={
-          <Button
-            type="positive"
-            text={"긍정 버튼"}
-            onClick={() => {
-              alert("positive button")
-            }}
-          />
-        }
-        rightChild={
-          <Button
-            type="negative"
-            text={"부정 버튼"}
-            onClick={() => {
-              alert("negative button")
-            }}
-          />
-        }
-      />
-      <Editor
-        initData={{
-          date: new Date().getTime(),
-          emotionId: 1,
-          content: "이전에 작성했던 일기"
-        }}
-        onSubmit={() => {
-          alert("작성 완료!")
-        }}
-      />
-    </div>
+   <div>Home 페이지입니다.</div>
   )
 }
 export default Home
