960247b58bd9a121b15146b5380dfdf5b4943770
kimminhyuck <rhkddka02@naver.com>
2025-01-03 14:54:11 +0900

    리엑트 챔터 3 커밋&푸쉬

---
+++ circle.js
@@ -0,0 +1,21 @@
+// export const PI = 3.141592;
+
+// export function getArea(redius) {
+//     return PI * redius * redius;
+// }
+
+// export function getCircumference(redius) {
+//     return 2 * PI * redius;
+// }
+
+const PI = 3.141592;
+function getArea(redius) {
+    return PI * redius * redius;
+}
+function getCircumference(redius) {
+    return 2 * PI * redius;
+}
+
+// export { PI, getArea, getCircumference };
+
+export default { PI, getArea, getCircumference };
